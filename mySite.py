f05b85be915f52c2a53cd30cad8aa5c7cf01af7c
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-08-21 13:37:32 -0300

    first commit

---
+++ mySite.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# construct pagine of site
+
+# route -> monitoramento.com/Temperatura
+# funcao  -> o que será exibido
+@app.route("/<valueTemperature>")
+def homepage(valueTemperature):
+    return render_template("index.html", valueTemp=valueTemperature)
+
+@app.route("/contato")
+def contato():
+    return "renan.saraiva@academico.ifpb.edu.br"
+
+# run site
+if __name__ == "__main__":
+    app.run(debug=True)
+
+app.run(debug=True)
